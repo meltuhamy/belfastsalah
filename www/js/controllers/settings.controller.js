48ea8c78075999aa345894ac93f3727b5d8de53a
Mohamed Eltuhamy <tuhamy@gmail.com>
2015-03-11 09:04:37 +0000

    Add about modal, improve styles, add ngcordova

--- www/js/controllers/settings.controller.js
+++ www/js/controllers/settings.controller.js
@@ -1,3 +1,22 @@
-belfastsalah.controllers.controller('SettingsCtrl', function($scope) {
-  
+belfastsalah.controllers.controller('SettingsCtrl', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('templates/modal-about.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
 });
