0486c29837281f3e875526403442fa3cf7feb652
Mohamed Eltuhamy <tuhamy@gmail.com>
2015-03-19 14:19:37 +0000

    Add today controller tests

--- test/unit/controllers/today.controller.test.js
+++ test/unit/controllers/today.controller.test.js
@@ -1,14 +1,33 @@
 describe('TodayCtrl', function(){
   beforeEach(module('belfastsalah'));
 
-  var ctrl, scope;
+  var ctrl, scope, PrayerTimes;
 
-  beforeEach(inject(function($controller, $rootScope){
+  beforeEach(inject(function($controller, $rootScope, _PrayerTimes_){
     scope = $rootScope.$new();
+
+    PrayerTimes = _PrayerTimes_;
+
+    spyOn(PrayerTimes, 'getByDate').and.callThrough();
+    spyOn(PrayerTimes, 'getNextPrayer').and.callThrough();
+
     ctrl = $controller('TodayCtrl', { $scope: scope });
   }));
 
-  it('should work', function(){
-    expect(true).toBe(true);
+  it('should get initial prayer time values for today date', function(){
+    expect(scope.tickerDate instanceof Date).toBe(true);
+    expect(scope.tickerPrayerTimes).toBeDefined();
+    expect(scope.nextPrayer).toBeDefined();
+    expect(PrayerTimes.getByDate).toHaveBeenCalled();
+    expect(PrayerTimes.getNextPrayer).toHaveBeenCalled();
+
   });
+
+  it('should update prayer times on tick events', inject(function(EVENTS){
+    var date = new Date();
+    scope.$emit(EVENTS.TICK, date);
+    expect(scope.tickerDate).toBe(date);
+    expect(PrayerTimes.getByDate).toHaveBeenCalledWith(date);
+    expect(PrayerTimes.getNextPrayer).toHaveBeenCalled();
+  }));
 });
