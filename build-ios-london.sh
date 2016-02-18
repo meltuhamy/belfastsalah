d98f581e71dc8cd21b0558957487f4086b840ef2
meltuhamy <tuhamy@gmail.com>
2016-02-18 20:20:48 +0000

    fix ios build scripts

--- build-ios-london.sh
+++ build-ios-london.sh
@@ -1,7 +1,4 @@
 git checkout london
-#keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
 gulp
 npm test
-cordova build --release android
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore belfast.keystore ./platforms/android/ant-build/MainActivity-release-unsigned.apk alias_name
-rm -f Release.apk && zipalign -v 4 ./platforms/android/ant-build/MainActivity-release-unsigned.apk Release.apk
+cordova build --release ios
