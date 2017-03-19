28a2ad7f4902bd0accc8a76d3751ab0912c640c3
meltuhamy <tuhamy@gmail.com>
2017-03-19 15:39:22 +0000

    settings migration

--- src/app/defaultSettings.ts
+++ src/app/defaultSettings.ts
@@ -3,12 +3,32 @@
  * @returns {boolean}
  */
 function isMigration(){
-  return !!(window.localStorage.getItem('belfastsalah_setting::showDisclaimer') || window.localStorage.getItem('belfastsalah_setting::showTrackingDisclaimer'));
+  try {
+    return !!(window.localStorage.getItem('belfastsalah_setting::showDisclaimer') || window.localStorage.getItem('belfastsalah_setting::showTrackingDisclaimer'));
+  } catch (e){
+    return false;
+  }
+}
+
+function getLegacySetting(settingName : string, defaultSetting : any){
+  try{
+    let item = window.localStorage.getItem(`belfastsalah_setting::${settingName}`);
+    if(item){
+      return JSON.parse(item);
+    } else {
+      return defaultSetting;
+    }
+  } catch (e){
+    return defaultSetting;
+  }
 }
 
 export const defaultSettings = {
+  notifications: getLegacySetting('notifications', true),
+  notifyMinutes: getLegacySetting('notifyMinutes', 5),
+  hanafiAsr: getLegacySetting('hanafiAsr', false),
+  showDisclaimer: getLegacySetting('showDisclaimer', true),
+  nightMode: getLegacySetting('nightMode', false),
+  showTrackingDisclaimer: getLegacySetting('showTrackingDisclaimer', true),
+  location: isMigration() ? 'london' : null
 };
-
-if(isMigration()){
-  defaultSettings['location'] = 'london';
-}
