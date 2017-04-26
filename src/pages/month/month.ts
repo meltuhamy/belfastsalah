ac9aed641af53888914180866dc649fdff0a04aa
meltuhamy <tuhamy@gmail.com>
2017-04-26 08:29:52 +0100

    start work on month view

--- src/pages/month/month.ts
+++ src/pages/month/month.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import {PrayerTimeDay, PrayerTimes, PrayerTimesTable} from "../../providers/prayertimes";
 
 @Component({
   selector: 'page-month',
   templateUrl: 'month.html'
 })
 export class MonthPage {
+  currentMonth: PrayerTimeDay[];
 
-  constructor(public navCtrl: NavController) {
+  prayerTimesTable: PrayerTimesTable;
 
+  constructor(public navCtrl: NavController, public prayerTimesService : PrayerTimes) {
+    this.loadTimeTable().then(() => {
+      this.currentMonth = this.prayerTimesTable.getByMonth(new Date().getMonth()).filter(i => !!i);
+      console.log(this.currentMonth);
+    });
   }
+
+  loadTimeTable(){
+    return this.prayerTimesService.getTimeTable().then(prayerTime => {
+      this.prayerTimesTable = prayerTime;
+    });
+  }
+
 
 }
