cc586b012aa19be6c023172bc9cdd5f7a650ae75
meltuhamy <tuhamy@gmail.com>
2017-05-07 08:59:27 +0100

    Auto-enable night mode at maghrib and disable at shuruq

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { Settings } from '../providers/settings';
 import { Deploy } from '@ionic/cloud-angular';
 import {Notifications} from "../providers/notifications";
+import {PrayerTimes, tick} from "../providers/prayertimes";
 
 @Component({
   templateUrl: 'app.html'
@@ -13,8 +14,9 @@ import {Notifications} from "../providers/notifications";
 export class PrayerTimesApp {
   rootPage = TabsPage;
   settings: any;
+  nextPrayerType: string;
 
-  constructor(public platform: Platform, settings: Settings, public deploy: Deploy, public notifications : Notifications) {
+  constructor(public platform: Platform, settings: Settings, public deploy: Deploy, public notifications : Notifications, public prayerTimes : PrayerTimes) {
     settings.load()
       .then(() => platform.ready())
       .then(() => {
@@ -23,6 +25,7 @@ export class PrayerTimesApp {
         this.checkForLatestAppVersion();
         this.scheduleNotifications();
         this.settings = settings.allSettings;
+        this.initialisePrayerTimes();
       });
   }
 
@@ -41,6 +44,25 @@ export class PrayerTimesApp {
   scheduleNotifications(){
     if(this.platform.is('cordova')){
       return this.notifications.schedule({showToast: true});
+    }
+  }
+
+  initialisePrayerTimes(){
+    this.prayerTimes.getTimeTable().then(prayerTimes => {
+      const {next} = prayerTimes.getNextAndPrevPrayer();
+      this.nextPrayerType = next.type;
+      tick.subscribe(value => {
+        const {next} = prayerTimes.getNextAndPrevPrayer();
+        this.nextPrayerType = next.type;
+      });
+    })
+  }
+
+  isNightMode(nightMode: boolean, nightModeMaghrib: boolean, nextPrayerType : string): boolean{
+    if(nightMode && nightModeMaghrib){
+      return ['isha', 'fajr', 'shuruq'].indexOf(nextPrayerType) >= 0;
+    } else {
+      return nightMode;
     }
   }
 }
